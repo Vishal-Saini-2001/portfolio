48f7f4fbd43df80bd5570c5334d5a834fef19826
Vishal Saini <vishalsaini3054@gmail.com>
2024-08-16 19:26:05 +0530

    Resume Download Button Added

--- src/comps/Resume.js
+++ src/comps/Resume.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
-import resume from './MyCV.pdf';
-import { Worker } from '@react-pdf-viewer/core';
+import React from 'react'
+import resume from './mycv.pdf';
+import { Position, Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const Resume = () => {
 
+    const downloadBtnStyle = {
+        backgroundColor:"#443266",
+        padding:"7px 15px",
+        color:"white",
+        border:"none",
+        fontSize:"18px",
+        position:"absolute",
+        left:"45%"
+    }
+
     return (
-        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-            <Viewer fileUrl={resume} />
-        </Worker>
+        <>
+            <br/>
+            <a href={resume} download="VishalSainiCV">
+                <button type='button' style={downloadBtnStyle}>Download <i class="fa fa-download"></i></button>
+            </a>
+            <br/>
+            <br/>
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                <Viewer fileUrl={resume} />
+            </Worker>
+        </>
     )
 }
 
