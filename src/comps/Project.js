6b9f415f5d62bb6584c14ca821415e93ef659bc9
Vishal <vishalsaini3054@gmail.com>
2026-08-06 14:35:32 +0530

    fix alt tags

--- src/comps/Project.js
+++ src/comps/Project.js
@@ -12,23 +12,23 @@ const Project = () => {
             <h1 className='sec3-h1'>My <span>Projects</span></h1>
             <div className='projects-grid'>
                 <div className='project' data-aos="zoom-in" onClick={() => window.open("https://shareincentivecalculator.com")}>
-                    <img src={p4} />
+                    <img src={p4} alt="Share Incentive Plan Calculator" />
                     <h3><span>Share Incentive</span> Plan Calculator </h3>
                 </div>
                 <div className='project' data-aos="zoom-in" onClick={() => window.open("https://github.com/Vishal-Saini-2001/ResumeBuilder-frontend")}>
-                    <img src={p1} />
+                    <img src={p1} alt="Resume Builder" />
                     <h3><span>Resume</span> Builder</h3>
                 </div>
                 <div className='project' data-aos="zoom-in" onClick={() => window.open("https://github.com/Vishal-Saini-2001/PracticeProjects-ShoeStoreWebsite")}>
-                    <img src={p2} />
+                    <img src={p2} alt="Shoe Store Website" />
                     <h3><span>Shoe</span> Store</h3>
                 </div>
                 <div className='project' data-aos="zoom-in" onClick={() => window.open("https://github.com/Vishal-Saini-2001/BatchSystem-Vishal")}>
-                    <img src={p5} />
+                    <img src={p5} alt="Batch System Project" />
                     <h3><span>Batch</span> System</h3>
                 </div>
                 <div className='project' data-aos="zoom-in">
-                    <img src={p3} />
+                    <img src={p3} alt="Food Ordering Website" />
                     <h3><span>Food</span> Ordering Website</h3>
                 </div>
             </div>
