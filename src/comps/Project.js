90c29cb0823ef5eb184eb57eecdca2849109bd01
Vishal <vishalsaini3054@gmail.com>
2026-09-20 20:27:48 +0530

    p2 error fix

--- src/comps/Project.js
+++ src/comps/Project.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../css/Project.css'
 import p1 from '../assets/p1.png'
-import p2 from '../assets/p2.png'
+// import p2 from '../assets/p2.png'
 import p3 from '../assets/p3.png'
 import p4 from '../assets/p4.png'
 import p5 from '../assets/p5.png'
